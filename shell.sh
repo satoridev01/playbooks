40c12d3467d7d1d2279ba99d9427f0247acd0ba6
fearcito <82276470+fearcito@users.noreply.github.com>
2024-08-09 20:18:51 -0300

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -2,5 +2,5 @@
 #trap "" HUP INT
 EXTERNAL_IP="`curl -s ifconfig.me`"
 RANDOM_PORT=$((1024 + ($(date +%s%N) % 64511)))
-satori run shell.yml --data="{\"IP\":\"$EXTERNAL_IP\", \"PORT\":\"$RANDOM_PORT\"}"
+satori run satori://shell.yml --data="{\"IP\":\"$EXTERNAL_IP\", \"PORT\":\"$RANDOM_PORT\"}"
 nc -vl $RANDOM_PORT
